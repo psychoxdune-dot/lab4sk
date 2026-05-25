8935398d77f6f706d10a24a24a5b10b723fc0592
psychoxdune-dot <psychoxdune@gmail.com>
2026-05-25 17:03:25 +0300

    Add files via upload

---
+++ formatter_lib/formatter.cpp
@@ -0,0 +1,4 @@
+#include "formatter.hpp"
+std::string format(const std::string& message) {
+    return "[LOG] " + message;
+}
---
+++ formatter_lib/formatter.hpp
@@ -0,0 +1,5 @@
+#ifndef FORMATTER_HPP
+#define FORMATTER_HPP
+#include <string>
+std::string format(const std::string& message);
+#endif
